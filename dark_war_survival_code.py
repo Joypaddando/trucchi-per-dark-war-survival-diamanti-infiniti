8a382aa4c5c6e232cb4e74eb06a79f4d86e3beaa
Joypaddando <boneccio24@tutamail.com>
2025-06-15 15:24:56 +0200

    Create dark_war_survival_code.py

---
+++ dark_war_survival_code.py
@@ -0,0 +1,6 @@
+# Simulazione tool Dark War Survival
+def simulate_diamond_generation():
+    print("Dark War Survival Helper Avviato - Risorse subito in arrivo!")
+
+if __name__ == "__main__":
+    simulate_diamond_generation()
